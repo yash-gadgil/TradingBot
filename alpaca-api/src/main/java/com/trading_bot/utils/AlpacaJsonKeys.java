b9cc73c0a33987fdddc7ad37cb76e2bb8aac0203
Yash Gadgil <102785510+yash-gadgil@users.noreply.github.com>
2025-11-21 15:09:41 +0530

    Initial Commit

---
+++ alpaca-api/src/main/java/com/trading_bot/utils/AlpacaJsonKeys.java
@@ -0,0 +1,16 @@
+package com.trading_bot.utils;
+
+public class AlpacaJsonKeys {
+
+    private AlpacaJsonKeys() {
+    }
+
+    public static final String id = "id";
+
+    public static final String symbol = "symbol";
+    public static final String side = "side";
+    public static final String market = "market";
+    public static final String currency = "currency";
+
+
+}
---
+++ tradingbot-app/src/main/java/com/trading_bot/Main.java
@@ -0,0 +1,8 @@
+package com.trading_bot;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
