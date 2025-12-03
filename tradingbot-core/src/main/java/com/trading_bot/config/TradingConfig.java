1b3e5e55745253cd46881253ccf9a99c0d951fa7
Yash Gadgil <102785510+yash-gadgil@users.noreply.github.com>
2025-12-03 20:42:06 +0530

    TradingBot Core - Utils and Config additions

--- tradingbot-core/src/main/java/com/trading_bot/config/TradingConfig.java
+++ tradingbot-core/src/main/java/com/trading_bot/config/TradingConfig.java
@@ -1,4 +1,14 @@
 package com.trading_bot.config;
 
-public class TradingConfig {
+public class TradingConfig extends BaseTradingConfig {
+
+    private String mailTo;
+
+    public String getMailTo() {
+        return mailTo;
+    }
+
+    public void setMailTo(String mailTo) {
+        this.mailTo = mailTo;
+    }
 }
